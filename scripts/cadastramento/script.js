7c23f14c43da499e0ec5dcecab70858961538895
Alesson dos Santos Barros <alessonbarros@corenrj.local>
2024-07-30 08:26:00 -0300

    ....

--- scripts/cadastramento/script.js
+++ scripts/cadastramento/script.js
@@ -1,1 +1,30 @@
 //script
+document.addEventListener('DOMContentLoaded', (event) => {
+    const images = [
+        '../Assets/img/img01.jpg',  // substitua pelos caminhos das suas imagens
+        '../Assets/img/img02.jpg',
+        '../Assets/img/img03.jpg'
+    ];
+    
+    let currentIndex = 0;
+    
+    const btn = document.getElementById('btn');
+    const imageContainer = document.getElementById('imageContainer');
+    
+    btn.addEventListener('click', () => {
+        // Remove a imagem atual se houver
+
+        console.log("esntrei")
+        while (imageContainer.firstChild) {
+            imageContainer.removeChild(imageContainer.firstChild);
+        }
+        
+        // Adiciona a próxima imagem
+        const img = document.createElement('img');
+        img.src = images[currentIndex];
+        imageContainer.appendChild(img);
+        
+        // Atualiza o índice
+        currentIndex = (currentIndex + 1) % images.length;
+    });
+});
